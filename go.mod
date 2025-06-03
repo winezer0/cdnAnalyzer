6ac271a560361db1d27b78bb837ad858dd8b1d39
WINEZERO <46115146+winezer0@users.noreply.github.com>
2025-06-04 00:39:33 +0800

    增加ip查询

--- go.mod
+++ go.mod
@@ -4,7 +4,11 @@ go 1.23.0
 
 toolchain go1.23.9
 
-require github.com/miekg/dns v1.1.66
+require (
+	github.com/ipipdotnet/ipdb-go v1.3.3
+	github.com/miekg/dns v1.1.66
+	golang.org/x/text v0.25.0
+)
 
 require (
 	golang.org/x/mod v0.24.0 // indirect
---
+++ iplocate/qqwryipdb/qqwryipdb_test.go
@@ -0,0 +1,32 @@
+package qqwryipdb
+
+import (
+	"testing"
+)
+
+func init() {
+	if err := LoadFile("C:\\Users\\WINDOWS\\Desktop\\CDNCheck\\qqwry.dat"); err != nil {
+		panic(err)
+	}
+}
+
+func TestQueryIP(t *testing.T) {
+	queryIp := "119.29.29.29"
+	location, err := QueryIP(queryIp)
+	if err != nil {
+		t.Fatal(err)
+	}
+	emptyVal := func(val string) string {
+		if val != "" {
+			return val
+		}
+		return "未知"
+	}
+	t.Logf("国家：%s，省份：%s，城市：%s，区县：%s，运营商：%s",
+		emptyVal(location.Country),
+		emptyVal(location.Province),
+		emptyVal(location.City),
+		emptyVal(location.District),
+		emptyVal(location.ISP),
+	)
+}
