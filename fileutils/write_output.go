3c8b7c6ee3a2f4e61d52c209ba6932df58343552
WINEZERO <46115146+winezer0@users.noreply.github.com>
2025-06-22 01:16:59 +0800

    Update write_output.go

--- fileutils/write_output.go
+++ fileutils/write_output.go
@@ -4,17 +4,16 @@ import (
 	"cdnCheck/maputils"
 	"fmt"
 	"os"
-	"path/filepath"
 	"strings"
 )
 
-func WriteOutputToFile(data interface{}, outputType, outputFile, targetFile string) error {
+func WriteOutputToFile(data interface{}, outputType, outputFile string) error {
 	var err error
 
 	switch strings.ToLower(outputType) {
 	case "csv":
 		if outputFile == "" {
-			outputFile = getDefaultFilename(targetFile, ".csv")
+			outputFile = "result.csv"
 		}
 		sliceToMaps, mapErr := maputils.ConvertStructsToMaps(data)
 		if mapErr == nil {
@@ -25,13 +24,13 @@ func WriteOutputToFile(data interface{}, outputType, outputFile, targetFile stri
 
 	case "json":
 		if outputFile == "" {
-			outputFile = getDefaultFilename(targetFile, ".json")
+			outputFile = "result.json"
 		}
 		err = os.WriteFile(outputFile, maputils.AnyToJsonBytes(data), 0644)
 
 	case "txt":
 		if outputFile == "" {
-			outputFile = getDefaultFilename(targetFile, ".txt")
+			outputFile = "result.txt"
 		}
 		err = WriteText(outputFile, maputils.AnyToTxtStr(data))
 
@@ -45,14 +44,4 @@ func WriteOutputToFile(data interface{}, outputType, outputFile, targetFile stri
 	}
 	fmt.Printf("结果已写入%s文件: %s\n", outputType, outputFile)
 	return nil
-}
-
-// 辅助函数
-func getDefaultFilename(base, suffix string) string {
-	if base == "" {
-		return ""
-	}
-	ext := filepath.Ext(base)
-	name := base[:len(base)-len(ext)]
-	return name + ".check" + suffix
 }
